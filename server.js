58f52db03313e879127fcc64ed3811a10dc058d7
OmbongiMN <ombongim391@gmail.com>
2023-04-11 02:13:46 +0300

    Serverjs changes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+//let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = 'mongodb+srv://ombongiMN:pvs4JxtGoTA3joFq@cluster0.p3mvbu5.mongodb.net/?retryWrites=true&w=majority';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
